021dc94fb39b2f9f5bdbee9f3620f3f317323f2f
MarcioBertani <mbertani90@gmail.com>
2024-05-16 15:05:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 '''
+# Pedido do cliente: 
+
 Tenho uma planilha no excel com os dados dos alunos que fizeram um curso.
 
 Quero ver a possibilidade de criar um programa usando o Python
@@ -10,28 +12,22 @@ do inicio, data do final, carga horaria, data da emissao do certificado
 e as assinaturas do Gestor Geral.
 '''
 
-# Pegar os dados da planilha
-
 import openpyxl
 from PIL import Image, ImageDraw, ImageFont
 
-# Abrir a planilha
 workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
 sheet_alunos = workbook_alunos['Sheet1']
 
 for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)): 
 
-    # acessar cada celula que contem a info que precisamos
-    nome_curso = linha[0].value # Acessar o nome do curso
-    nome_participante = linha[1].value # nome do participante
-    tipo_participacao = linha[2].value # tipo de participacao
-    data_inicio = linha[3].value # data do inicio
-    data_termino = linha[4].value # data do termino
-    carga_horaria = linha[5].value # carga horaria
-    data_emissao = linha[6].value # data de emissao
+    nome_curso = linha[0].value 
+    nome_participante = linha[1].value
+    tipo_participacao = linha[2].value 
+    data_inicio = linha[3].value
+    data_termino = linha[4].value 
+    carga_horaria = linha[5].value 
+    data_emissao = linha[6].value 
 
-    # Transferir os dados da planilha para a imagem do certificado
-    # Definir fonte a ser usada
     font_nome = ImageFont.truetype('./tahomabd.ttf',80)
     font_geral = ImageFont.truetype('./tahoma.ttf',90)
     font_data = ImageFont.truetype('./tahoma.ttf',55)
